97b3853701fe8e81d97cb80e520d18434cf0f665
Galiez Thomas <thomas.galiez18@gmail.com>
2020-07-13 15:39:15 +0200

    First site created in HTML and CSS without Bootstrap

---
+++ CV.js
@@ -0,0 +1,64 @@
+const sr = ScrollReveal();
+
+sr.reveal('.title', {
+    duration: 1500,
+    origin: 'top',
+    distance: '500%'
+});
+
+sr.reveal('.resume', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '1000%'
+});
+
+sr.reveal('#infosPerso', {
+    duration: 2500,
+    origin: 'left',
+    distance: '100%'
+});
+
+sr.reveal('.descrip', {
+    duration: 2500,
+    origin: 'right',
+});
+
+sr.reveal('#icons', {
+    duration: 3000,
+});
+
+sr.reveal('#proservia', {
+    duration: 2500,
+    origin: 'left',
+    distance: '100%',
+});
+
+sr.reveal('#helpline', {
+    duration: 2500,
+    origin: 'left',
+    distance: '100%',
+});
+
+sr.reveal('#duacom', {
+    duration: 2500,
+    origin: 'left',
+    distance: '100%',
+});
+
+sr.reveal('#teleperformance', {
+    duration: 2500,
+    origin: 'left',
+    distance: '100%',
+});
+
+sr.reveal('#iconsHobbies', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '100%',
+});
+
+sr.reveal('#contact', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '100%',
+});
